55e79bb1c70a575af93b3597e95b9aa86696f6d6
Tiago Peralta <tperalta82@gmail.com>
2017-11-19 08:40:30 +0000

    updated gitignore, and deps script

---
+++ scripts/install_deb_builddeps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Run as root";
+echo "Tested on Debian Stretch"
+apt-get install git-core build-essential libgmp-dev libmysqlclient-dev default-libmysqlclient-dev libcurl4-openssl-dev
